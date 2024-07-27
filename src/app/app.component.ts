00272ad5707e058fc9b59c3e51c64944cd2881a5
fthornton67 <fthornton67@gmail.com>
2024-07-27 09:24:33 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  providers:[ {provide: APP_BASE_HREF, useValue: environment.baseUrl}], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
