ad912674cb1a1ed4ce2de261a582271fc31ba8ef
fthornton67 <fthornton67@gmail.com>
2024-07-29 08:02:30 -0400

    remove page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './modules/shared/page-not-found/page-not-found.component';
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', pathMatch:"prefix", loadChildren: () => import('./modules/welcome/welcome.module').then(m => m.WelcomeModule) },
   { path: 'landing',  pathMatch:"prefix",loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) },
   { path: 'profile',  pathMatch:"prefix",loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule) },
